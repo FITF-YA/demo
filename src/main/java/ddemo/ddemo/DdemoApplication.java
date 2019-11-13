5a28dae49277424692e778dba6a17f8e4d8923c9
FITF-YA <1265915620@qq.com>
2019-11-14 01:01:59 +0800

    seconds

---
+++ src/main/java/ddemo/ddemo/DdemoApplication.java
@@ -0,0 +1,14 @@
+package ddemo.ddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdemoApplication.class, args);
+        System.out.println("运行成功...");
+    }
+
+}
